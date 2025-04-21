0842c31ce0ca6d5975d7de4442cc17bf775a1618
Rama <rassyabr79@gmail.com>
2025-04-21 14:00:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
+// File: api/server.js
 const express = require("express");
-require("dotenv").config();
-const fileRoutes = require("./routes/fileRoutes.js");
+const fileRoutes = require("../routes/fileRoutes.js");
 const path = require("path");
+const serverless = require("serverless-http"); // WAJIB
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Routes
 app.use("/", fileRoutes);
 
 // Error handling routes
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).sendFile(path.join(__dirname, "public", "500.html"));
+  res.status(500).sendFile(path.join(__dirname, "../public", "500.html"));
 });
 
-// security
+// Security headers
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
@@ -29,6 +29,5 @@ app.use((req, res, next) => {
   next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running at ${process.env.PUBLIC_URL}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
